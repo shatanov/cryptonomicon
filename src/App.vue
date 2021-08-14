d6e6acbbe17354b99fc50fd82e072c4a2adb8f35
Timur Shatanov <timur-shatanov.work@mail.ru>
2021-08-14 19:57:10 +0300

    добавлен компонент addButton

--- src/App.vue
+++ src/App.vue
@@ -27,63 +27,42 @@
       </div>
     </template>
     <div class="container">
-      <section>
-        <div class="flex">
-          <div class="max-w-xs">
-            <label for="wallet" class="block text-sm font-medium text-gray-700"
-              >Тикер</label
+      <div class="flex">
+        <div class="max-w-xs">
+          <label for="wallet" class="block text-sm font-medium text-gray-700"
+            >Тикер</label
+          >
+          <div class="mt-1 relative rounded-md shadow-md">
+            <input
+              @keyup.enter="addTicker"
+              @input="addHint"
+              v-model="ticker"
+              type="text"
+              name="wallet"
+              id="wallet"
+              class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+              placeholder="Например DOGE"
+            />
+          </div>
+          <div
+            v-show="hintState"
+            class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
+          >
+            <span
+              class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+              v-for="hint in cardHintState"
+              :key="hint.FullName"
+              @click="addTicker(hint)"
             >
-            <div class="mt-1 relative rounded-md shadow-md">
-              <input
-                @keyup.enter="addTicker"
-                @input="addHint"
-                v-model="ticker"
-                type="text"
-                name="wallet"
-                id="wallet"
-                class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                placeholder="Например DOGE"
-              />
-            </div>
-            <div
-              v-show="hintState"
-              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
-            >
-              <span
-                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
-                v-for="hint in cardHintState"
-                :key="hint.FullName"
-                @click="addTicker(hint)"
-              >
-                {{ hint.Symbol }}
-              </span>
-            </div>
-            <div class="text-sm text-red-600" v-show="cardReplayState">
-              Такой тикер уже добавлен
-            </div>
+              {{ hint.Symbol }}
+            </span>
+          </div>
+          <add-button @click="addTicker" />
+          <div class="text-sm text-red-600" v-show="cardReplayState">
+            Такой тикер уже добавлен
           </div>
         </div>
-        <button
-          @click="addTicker"
-          type="button"
-          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-        >
-          <!-- Heroicon name: solid/mail -->
-          <svg
-            class="-ml-0.5 mr-2 h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="#ffffff"
-          >
-            <path
-              d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-            ></path>
-          </svg>
-          Добавить
-        </button>
-      </section>
+      </div>
 
       <template v-if="tickerCards.length">
         <hr class="w-full border-t border-gray-600 my-4" />
@@ -157,10 +136,9 @@
         >
           <div
             v-for="(bar, idx) in normalizeGraph"
-            ref="graphElement"
             :key="idx"
-            :style="{ height: `${bar}%` }"
-            class="bg-purple-800 border h-24 test"
+            :style="{ height: `${bar}%`, width: `${this.graphElementWidth}px` }"
+            class="bg-purple-800 border h-24"
           ></div>
         </div>
         <button
@@ -197,9 +175,15 @@
 
 <script>
 import { subscribeToTicker, unsubscribeFromTicker } from "./api";
+import AddButton from "./components/AddButton.vue"
 
 export default {
   name: "App",
+
+  components: {
+    AddButton
+  },
+
   data() {
     return {
       ticker: null,
@@ -215,7 +199,7 @@ export default {
       cardReplayState: false,
       hintState: false,
 
-      graphElementWidth: undefined,
+      graphElementWidth: 38,
       page: 1,
       maxGraphElements: 1,
     };
@@ -312,7 +296,7 @@ export default {
         .forEach((ticker) => {
           if (ticker === this.cardState) {
             this.graphState.push(price);
-            this.updateGraph()
+            this.updateGraph();
           }
           price === undefined
             ? (ticker.tickerExistence = false)
@@ -339,12 +323,8 @@ export default {
         return;
       }
 
-      // if (!this.graphElementWidth) {
-      //   this.graphElementWidth = this.$refs.graphElement.clientWidth;
-      // }
-
       this.maxGraphElements =
-        this.$refs.graph.clientWidth / 38
+        this.$refs.graph.clientWidth / this.graphElementWidth;
     },
 
     addTicker(hint) {
@@ -416,7 +396,7 @@ export default {
     },
 
     maxGraphElements() {
-      this.updateGraph()
+      this.updateGraph();
     },
 
     paginatedTickers() {
